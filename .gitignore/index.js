d0d17e138054e371eca43f7319c9a808102fc697
RomainC17 <55240335+RomainC17@users.noreply.github.com>
2020-07-23 00:05:18 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-
 /*     STATUT DU BOT      */
 bot.on('ready', function (){
     bot.user.setActivity('GRHelp - En cours de dev - Griffon#4905'); // Son activité
@@ -12,27 +11,76 @@ bot.on('ready', function (){
 
 /*     RÉACTION AUX COMMANDES      */
 bot.on('message', function (message){
-    if (message.content === 'GRtest') { //Commande de test
-      message.reply('OK ça marche !')
-    }
-    if (message.content === 'GRStatut') { //Commande de statut du bot
-      message.reply('BOT en cours de maintenance')
-    }
-    if (message.content === 'GRTcheaze') { //Commande FUN
-      message.reply('Ce mec est juste un gros noob')
-    }
-    if (message.content === 'GRNathanG') { //Commande FUN
-      message.reply('De toute évidence, ce mec est un bg')
-    }
-    if (message.content === 'GRGofi') { //Commande FUN
-      message.reply('OOOOHHH LUUUUUIIIII')
-    }
-    if (message.content === 'GRHelp') { //Commande FUN
-      message.reply('\n```diff\n-Utilise le préfixe : GR puis à la suite le nom de ta commande :\n``````\n-Test : le bot doit te répondre``````\n-Statut : Informe sur le statut du bot``````\n-Tcheaze / NathanG / Gofi : ;)``` ')
-    }
-  })
 
-/*     EXPULSER UN MEMBRE      */
+    if (message.content === 'GRTestB') { //Test
+      message.reply('OK c\'est bon pour moi !');
+    }
+    if (message.content === 'GREtatB') { //Statut du bot
+      message.reply('BOT en cours de maintenance');
+    }
+    if (message.content === 'GRNomS') { //Affiche le nom du serveur
+      message.channel.send('Le nom du serveur actuel est  : ' + message.guild.name);
+    }
+    if (message.content === 'GRMembreS') { //Donne le nombre de membres sur le serveur
+      message.channel.send('Nombre total de personnes sur le serveur : ' + message.guild.memberCount + ' (toi y compris)');
+    }
+    if (message.content === 'GRInfosT') { //Donne les infos de l'utilisateur
+      message.channel.send('Ton pseudo : ' + message.author.username);
+      message.channel.send('Ton tag : ' + message.author.discriminator);
+      message.channel.send('Ta date de création de compte : ' + message.author.createdAt);      
+      message.channel.send('Ton identifiant : ' + message.author.id);
+    }
+    if (message.content === 'GRTcheaze') { //FUN
+      message.reply('Tcheaze à juste été littéralement plus de fois absent en PPE que casper ');
+    }
+    if (message.content === 'GRNathanG') { //FUN
+      message.reply('De toute évidence,NathanG est un bg');
+    }
+    if (message.content === 'GRGofi') { //FUN
+      message.reply('OOOOHHH LUUUUUIIIII');
+    }
+    if (message.content === 'GRHelp') { //HELP DE TOUTES LES COMMANDES
+      message.reply('\n```diff\n' + 
+      '-Utilise le préfixe : GR puis à la suite le nom de ta commande :' +
+      '\n```\n' + 
+      '__Commandes à propos **DU BOT** :__ ' +
+      '\n \n' +
+      '*-TestB* : Le bot doit te répondre (si c\'est pas le cas, c\'est la douille).' + 
+      '\n' + 
+      '*-EtatB* : Informe sur l\'état du bot.' + 
+      '\n \n' +
+      '__Commandes à propos **DU SERVEUR** :__' + 
+      '\n \n' +
+      '*-NomS* : Donne le nom du serveur actuel.' + 
+      '\n' + 
+      '*-MembreS* : Donne le nombre de personnes sur le serveur.' +
+      '\n \n' +
+      '__Commandes à propos **DE TOI** :__' + 
+      '\n \n' +
+      '*-InfosT* : Donne ton réel pseudo, ton tag, ta date de création de ton compte, ton id. ' +
+      '\n \n' + 
+      '__Commandes pour le **FUN** :__' +
+      '\n \n' +
+      '*-Tcheaze* / *NathanG* / *Gofi* : Surprise :)' + 
+      '');
+  }
+})
 
 
-bot.login('NzM1MTQwOTQxMTMxNjc3ODI3.Xxb8fw.71cYhJcL37Vl_iQA_VMUawUAQ00'); // TOKEN DU BOT
+
+
+
+
+
+
+/*     ATTENTION, A 10 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 9 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 8 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 7 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 6 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 5 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 4 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 3 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 2 LIGNES DU TOKEN DU BOT      */
+/*     ATTENTION, A 1 LIGNE DU TOKEN DU BOT      */
+bot.login('NzM1MTQwOTQxMTMxNjc3ODI3.Xxb8fw.71cYhJcL37Vl_iQA_VMUawUAQ00');
