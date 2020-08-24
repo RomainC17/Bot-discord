f56798733a603c651b57c6971864681a4fa0ce64
RomainC17 <55240335+RomainC17@users.noreply.github.com>
2020-08-25 01:31:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 
 /*     STATUT DU BOT      */
 bot.on('ready', function (){
-    bot.user.setActivity('GRHelp - En cours de dev - Griffon#4905'); // Son activité
+    bot.user.setActivity('GRHelp - By Griffon#4905'); // Son activité
     bot.user.setUsername('Griffon - BOT'); // Son nom qui s'affiche
 })
 
@@ -16,7 +16,7 @@ bot.on('message', function (message){
       message.reply('OK c\'est bon pour moi !');
     }
     if (message.content === 'GREtatB') { //Statut du bot
-      message.reply('BOT opérationnel !');
+      message.reply('BOT opérationnel');
     }
     if (message.content === 'GRNomS') { //Affiche le nom du serveur
       message.channel.send('Le nom du serveur actuel est  : ' + message.guild.name);
@@ -54,6 +54,16 @@ bot.on('message', function (message){
       '\n' + 
       '*-EtatB* : Informe sur l\'état du bot.' + 
       '\n \n' +
+      '__Commandes à propos **DE LA MUSIQUE** :__' + 
+      '\n \n' +
+      '*-Play* + url : Joue le titre demandé.' + 
+      '\n' + 
+      '*-Pause* : Met en pause la musique / Reprends la lecture.' +
+      '\n' +
+      '*-Skip* : Saute le titre joué pour passer au suivant.' +
+      '\n' +
+      '*-Stop* : Arrête la musique et déconnecte le bot.' +
+      '\n \n' +
       '__Commandes à propos **DU SERVEUR** :__' + 
       '\n \n' +
       '*-NomS* : Donne le nom du serveur actuel.' + 
@@ -69,7 +79,138 @@ bot.on('message', function (message){
       '*-Tcheaze* / *NathanG* / *Gofi* : Surprise :)' + 
       '');
   }
-})
+});
+
+const fetch = require('node-fetch');
+const ytdl = require("ytdl-core");
+const queue = new Map();
+
+bot.on("message", async message => {
+  let commande = message.content.trim().split(" ")[0].slice(1)
+  if (message.author.bot) return;
+  /*****************************************
+  ************ COMMANDE MUSIQUE ***********
+  ******************************************/
+  const serverQueue = queue.get(message.guild.id);
+  if (message.content.startsWith(`GRPlay`)) {
+      execute(message, serverQueue);
+      return;
+  } 
+  else if (message.content.startsWith(`GRSkip`)) {
+      skip(message, serverQueue);
+      return;
+  } 
+  else if (message.content.startsWith(`GRStop`)) {
+      stop(message, serverQueue);
+      return;
+  } 
+  else if (message.content.startsWith(`GRPause`)) {
+      pause(message, serverQueue);
+      return;
+  }
+});
+
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+      return message.channel.send(
+          "You need to be in a voice channel to play music!"
+      );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+      return message.channel.send(
+          "I need the permissions to join and speak in your voice channel!"
+      );
+  }
+
+  const songInfo = await ytdl.getInfo(args[1]);
+  const song = {
+      title: songInfo.title,
+      url: songInfo.video_url
+  };
+
+  if (!serverQueue) {
+      const queueContruct = {
+          textChannel: message.channel,
+          voiceChannel: voiceChannel,
+          connection: null,
+          songs: [],
+          volume: 5,
+          playing: true
+      };
+
+      queue.set(message.guild.id, queueContruct);
+
+      queueContruct.songs.push(song);
+
+      try {
+          var connection = await voiceChannel.join();
+          queueContruct.connection = connection;
+          play(message.guild, queueContruct.songs[0]);
+      } catch (err) {
+          console.log(err);
+          queue.delete(message.guild.id);
+          return message.channel.send(err);
+      }
+  } else {
+      serverQueue.songs.push(song);
+      return message.channel.send(`${song.title} has been added to the queue!`);
+  }
+}
+
+function skip(message, serverQueue, args) {
+  if (!message.member.voice.channel)
+      return message.channel.send(
+          "Tu dois être connecté au salon pour skip la musique !"
+      );
+  if (!serverQueue)
+      return message.channel.send("There is no song that I could skip!");
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+      return message.channel.send(
+          "Tu dois être connecté au salon pour stopper la musique !"
+      );
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+
+function pause(message, serverQueue, args) {
+  if (!message.member.voice.channel)
+      return message.channel.send(
+          "Tu dois être connecté au salon pour mettre en pause la musique !"
+      );
+  if (serverQueue.connection.dispatcher.paused) {
+      serverQueue.connection.dispatcher.resume();
+  } else {
+      serverQueue.songs = [];
+      serverQueue.connection.dispatcher.pause();
+  }
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+      serverQueue.voiceChannel.leave();
+      queue.delete(guild.id);
+      return;
+  }
+
+  const dispatcher = serverQueue.connection
+        .play(ytdl(song.url))
+        .on("finish", () => {
+            serverQueue.songs.shift();
+            play(guild, serverQueue.songs[0]);
+        })
+        .on("error", error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    serverQueue.textChannel.send(`Son joué : **${song.title}**`);
+}
+
 
 
 
